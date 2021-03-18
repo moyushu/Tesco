c015daa8e29d2e6ef4dd22db2e4efd7d2cf0b336
书墨 <zl2237309073@gmail.com>
2021-03-18 20:56:30 +0800

    文件结构

---
+++ scaffolding/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <div :style="bgImg"></div>
+  </div>
+</template>
+<style>
+.login * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      bgImg: {
+        backgroundImage: "url(../../public/images/bg.jpg)",
+        width: "100%",
+        height: "400px",
+      },
+    };
+  },
+};
+</script>
